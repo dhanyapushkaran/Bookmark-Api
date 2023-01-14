90df83e7f8d064bdb4b0d4b126eb7905ee13a480
AMT <dhanya@amt.in>
2023-01-14 22:06:22 +0530

    add promise to api

--- services/data.js
+++ services/data.js
@@ -1,6 +1,7 @@
 const db = require('./db')
 
 const createBookmark = async (args) => {
+    return new Promise((resolve,reject)=>{
     console.log("args", args)
     const newEntry = new db.Bookmark({
         title : args.title,
@@ -12,26 +13,29 @@ const createBookmark = async (args) => {
             console.log( error);
         } else {
             console.log('save success');
-         return {
+         return resolve ({
         statusCode: 200,
         status: true,
         message: "successfully Added"
-       }
+       })
         }
     });
+})
 }
 
 const listBookmark = () => {
-    return db.Bookmark.find().then((error, data)=>{ 
+    return new Promise((resolve,reject)=>{
+     db.Bookmark.find().then((data, error)=>{ 
+        console.log("data",data)
         if(data){
-        return {
+        return resolve ({
             statusCode: 200,
             status: true,
             data: data
-        }
+        })
     }
     else{
-        console.log(error)
+        console.log("err",error)
         return {
             statusCode: 422,
             status: false,
@@ -39,7 +43,7 @@ const listBookmark = () => {
         }
     }
     })
-   
+})
 }
 
 module.exports = { createBookmark , listBookmark}
